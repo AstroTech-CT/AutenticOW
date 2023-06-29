20ac90d0a28c887916ae5d911d04b413cb3ad620
Carolina <carolina.torres.baroffio@gmail.com>
2023-06-29 14:38:00 -0300

    Actualizar modelos de usuario y migraciones de la base de datos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('ci')->unique();
             $table->string('name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->string('username')->unique();
+            $table->boolean('reportado')->default(false);
+            $table->boolean('suspendido')->default(false);
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
